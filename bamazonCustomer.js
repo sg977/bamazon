401c6f09754df79f5ec039571bb86b4f8745281c
Siyi Guo <guosiyi1993@gmail.com>
2018-08-04 23:41:49 -0400

    draft

---
+++ bamazonCustomer.js
@@ -0,0 +1,60 @@
+// Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// The app should then prompt users with two messages.
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+
+
+var mysql = require("mysql");
+var inquirer = require("inquier");
+
+//create the connection information for the SQL database
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "root",
+
+    database: "bamazon_db"
+});
+
+//connect to the mysql server and sql database  
+connection.connect(function(err) {
+    if (err) throw err;
+    //run the start function after the connection is made to prompt the user
+    start();
+});
+
+
+//start function
+function start() {
+    inquirer
+    .prompt([
+        {
+            name:"itemId",
+            type:"input",
+            message: "what is the ID of the product you would like to buy"
+        },
+        {
+            name:"unitOfProduct",
+            type: "input",
+            message: "how many units of the product you would like to buy"
+        }
+    ])
+    
+    .then(function(answer) {
+        //when finished prompting, update the table 
+    })
+
+}
+
+
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
